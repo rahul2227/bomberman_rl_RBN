b62dc2e6e7a4cc5cd10ddef676e7f9e1c6902a11
rahul2227 <rahul2227.l115@gmail.com>
2024-09-18 21:53:31 +0530

    Updated the indexing logic and made event changes

--- agent_code/Q_learner/callbacks.py
+++ agent_code/Q_learner/callbacks.py
@@ -4,21 +4,21 @@ import random
 
 import numpy as np
 
-from agent_code.Q_learner.helpers import WALL_RIGHT, WALL_UP, WALL_DOWN, WALL_LEFT, ACTIONS
+from agent_code.Q_learner.helpers import WALL_RIGHT, WALL_UP, WALL_DOWN, WALL_LEFT, ACTIONS, wall_action_index
 
 
 def setup(self):
     q_table_folder = "q_tables/"
     self.new_state = None
     self.old_state = None
-    self.valid_state_list = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+    self.valid_state_list = ['WALL_UP', 'WALL_DOWN', 'WALL_LEFT', 'WALL_RIGHT', 'NO_OBSTACLE']
     if self.train:
         self.logger.info("Q-learning agent from scratch")
         self.number_of_states = len(self.valid_state_list)
         self.Q_table = np.zeros(shape=(self.number_of_states, len(ACTIONS)))  # currently 4 * 6
         # weights = np.random.rand(len(ACTIONS))
         # self.model = weights / weights.sum()
-        self.exploration_rate = 0.2  #random choice
+        self.exploration_rate = 0.8  #random choice
     else:
         self.logger.info("Loading q-table from saved state.")
         self.Q_table = load_q_table(self, q_table_folder)
@@ -32,16 +32,16 @@ def act(self, game_state: dict) -> str:
         self.old_state = self.new_state
 
     # Exploration vs exploitation
-    if self.train and random.random() < self.exploration_rate:
+    if self.train:
         self.logger.debug("Choosing action purely at random.")
         # taking a random action for exploration
         return np.random.choice(ACTIONS)
 
     self.logger.debug("Querying model for action.")
     # Querying the Q-table for an action
-    print(np.argmax(self.Q_table[self.old_state]))
-    action = np.random.choice(ACTIONS)
-    # action = ACTIONS[np.argmax(self.Q_table[self.old_state])]
+    # action = np.random.choice(ACTIONS)
+    action = ACTIONS[np.argmax(self.Q_table[self.old_state])]
+    self.logger.debug(f"action taken by the q-table model: {action}")
     return action
 
 
@@ -55,9 +55,9 @@ def state_to_features(self, game_state: dict) -> np.array:
     features['obstacles'] = obstacles
 
     # converting the features into a more indexable format for easy access in q-table
-    obstacle_state = ''.join(sorted([list(obstacle.keys())[0] for obstacle in obstacles])) or "NO_OBSTACLE"
+    obstacle_state = ''.join([str(obstacle) for obstacle in obstacles])
 
-    return obstacle_state
+    return int(obstacle_state)
 
 
 def load_q_table(self, q_table_folder):
@@ -82,12 +82,18 @@ def check_the_presence_of_obstacles(self, game_state: dict) -> np.array:
     wall_presence = {}
 
     if arena[coord_x + 1][coord_y] == 1 or (coord_x + 1, coord_y) == -1:
-        obstacle.append({'WALL_RIGHT': WALL_RIGHT})  # this will add Right wall presence in the list
+        # obstacle.append({'WALL_RIGHT': WALL_RIGHT})  # this will add Right wall presence in the list
+        obstacle.append(wall_action_index['WALL_RIGHT'])
     elif arena[coord_x - 1][coord_y] == 1 or (coord_x - 1, coord_y) == -1:
-        obstacle.append({'WALL_LEFT': WALL_LEFT})
+        # obstacle.append({'WALL_LEFT': WALL_LEFT})
+        obstacle.append(wall_action_index['WALL_LEFT'])
     elif arena[coord_x][coord_y + 1] == 1 or (coord_x, coord_y + 1) == -1:
-        obstacle.append({'WALL_UP': WALL_UP})
+        # obstacle.append({'WALL_UP': WALL_UP})
+        obstacle.append(wall_action_index['WALL_UP'])
     elif arena[coord_x][coord_y - 1] == 1 or (coord_x, coord_y - 1) == -1:
-        obstacle.append({'WALL_DOWN': WALL_DOWN})
+        # obstacle.append({'WALL_DOWN': WALL_DOWN})
+        obstacle.append(wall_action_index['WALL_DOWN'])
+    else:
+        obstacle.append(wall_action_index['NO_OBSTACLE'])
 
     return obstacle
--- agent_code/Q_learner/train.py
+++ agent_code/Q_learner/train.py
@@ -42,14 +42,14 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
         events.append(OBSTACLE_AVOID)
 
     # getting the state from the state_to_feature for old and new state
-    old_state_str = state_to_features(self, old_game_state)
-    new_state_str = state_to_features(self, new_game_state)
+    old_state_int = state_to_features(self, old_game_state)
+    new_state_int = state_to_features(self, new_game_state)
 
     # Initializing q-table entry
-    if old_state_str not in self.Q_table:
-        self.Q_table[old_state_str] = np.zeros(len(ACTIONS))
-    if new_state_str not in self.Q_table:
-        self.Q_table[new_state_str] = np.zeros(len(ACTIONS))
+    if old_state_int not in self.Q_table:
+        self.Q_table[old_state_int] = np.zeros(len(ACTIONS))
+    if new_state_int not in self.Q_table:
+        self.Q_table[new_state_int] = np.zeros(len(ACTIONS))
 
     # Mapping Action to index
     action_index = ACTIONS.index(self_action)
@@ -58,14 +58,14 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     reward = reward_from_events(self, events)
 
     # Q-value update
-    old_q_value = self.Q_table[old_state_str][action_index]
-    future_optimal = np.argmax(self.Q_table[new_state_str])
+    old_q_value = self.Q_table[old_state_int][action_index]
+    future_optimal = np.argmax(self.Q_table[new_state_int])
 
     # update Q-value
-    self.Q_table[new_state_str][action_index] = old_q_value + self.learning_rate * (reward + self.discount_rate * future_optimal - old_q_value)
+    self.Q_table[new_state_int][action_index] = old_q_value + self.learning_rate * (reward + self.discount_rate * future_optimal - old_q_value)
 
     # saving current state
-    self.old_state = new_state_str
+    self.old_state = new_state_int
 
     # transition logging
     self.transitions.append(
@@ -89,21 +89,21 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
     self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
 
     # Idea: Get Last state and action and update it in the q-table
-    last_state_str = state_to_features(self, last_game_state)
+    last_state_int = state_to_features(self, last_game_state)
     action_index = ACTIONS.index(last_action)
 
     # reward calculation
     reward = reward_from_events(self, events)
 
     # last game state in q-table
-    if last_state_str not in self.Q_table:
-        self.Q_table[last_state_str] = np.zeros(len(ACTIONS))
+    if last_state_int not in self.Q_table:
+        self.Q_table[last_state_int] = np.zeros(len(ACTIONS))
 
     # update
-    old_q_value = self.Q_table[last_state_str][action_index]
+    old_q_value = self.Q_table[last_state_int][action_index]
 
     # no future state, so future_reward=0
-    self.Q_table[last_state_str][action_index] = old_q_value + self.learning_rate * (reward - old_q_value)
+    self.Q_table[last_state_int][action_index] = old_q_value + self.learning_rate * (reward - old_q_value)
 
     # save the q-table
     q_table_file_path = "q_tables/q_table.npy"
@@ -125,9 +125,11 @@ def reward_from_events(self, events: List[str]) -> int:
     game_rewards = {
         e.COIN_COLLECTED: 1,
         e.KILLED_OPPONENT: 5,
-        PLACEHOLDER_EVENT: -.1,  # idea: the custom event is bad
-        OBSTACLE_HIT: 1,
-        OBSTACLE_AVOID: -.5,
+        OBSTACLE_HIT: -0.5,
+        OBSTACLE_AVOID: 1,
+        e.KILLED_SELF: -0.5,
+        e.BOMB_DROPPED: 0,
+        e.WAITED: -0.25,
     }
     reward_sum = 0
     for event in events:
